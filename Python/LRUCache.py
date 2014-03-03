28236d8a0352bbac1422fd985885fdee35f66876
Garrett Wang <guanyuwang.cs@gmail.com>
2014-03-03 00:07:29 -0500

    Update LRUCache.py

--- Python/LRUCache.py
+++ Python/LRUCache.py
@@ -1,16 +1,22 @@
 class LRUCache:
+
+    # @param capacity, an integer
     def __init__(self, capacity):
         self.__cacheMap = {}
         self.__cacheList = None
         self.__cacheListEnd = None
         self.__capacity = max(capacity, 1)
 
+    # @return an integer
     def get(self, key):
         node = self.__cacheMap.get(key)
         if node == None: return -1
         self.__moveToEnd(node)
         return node.v
 
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
     def set(self, key, value):
         node = self.__cacheMap.get(key)
         if node == None:
@@ -18,18 +24,9 @@ class LRUCache:
         else:
             node.v = value
             self.__moveToEnd(node)
-
+    
+    # add a new page
     def __addNew(self, k, v):
-        # first check whether remove LRU is needed
-        if len(self.__cacheMap) == self.__capacity:
-            del self.__cacheMap[self.__cacheList.k]
-            # modify the cache list
-            if self.__capacity == 1:
-                self.__cacheList = None
-                self.__cacheListEnd = None
-            else:
-                self.__cacheList.ne.pre = None
-                self.__cacheList = self.__cahceList.ne
         newNode = CacheNode(k, v, None, None)
         if self.__cacheList == None:
             self.__cacheList = newNode;
@@ -38,21 +35,28 @@ class LRUCache:
             newNode.pre = self.__cacheListEnd
         self.__cacheListEnd = newNode
         self.__cacheMap[k] = newNode
-
-    def __moveToEnd(self, node):
-        if node == self.__cacheListEnd: return
-        if node == self.__cacheList:
+        if len(self.__cacheMap) > self.__capacity:
+            del self.__cacheMap[self.__cacheList.k]
             self.__cacheList.ne.pre = None
             self.__cacheList = self.__cacheList.ne
+
+    # move a page to the end of the list
+    def __moveToEnd(self, node):
+        if node == self.__cacheListEnd: return
+        node.ne.pre = node.pre
+        if node == self.__cacheList:
+            self.__cacheList = node.ne
+        else:
+            node.pre.ne = node.ne
         self.__cacheListEnd.ne = node
         node.pre = self.__cacheListEnd
+        node.ne = None
         self.__cacheListEnd = node
-
+        
 class CacheNode:
     def __init__(self, k, v, pre, ne):
         self.k = k
         self.v = v
         self.pre = pre
         self.ne = ne
-
 
