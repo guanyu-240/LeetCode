e3788b28ad05fff05427ec6ba1171a007454c5d5
Guanyu Wang <guanyuwang.cs@gmail.com>
2015-01-17 00:44:26 -0500

    Create Largest_Number.cpp

---
+++ C++/Largest_Number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool compare(int n1, int n2){
+        string s1 = to_string(n1) + to_string(n2);
+        string s2 = to_string(n2) + to_string(n1);
+        int len = s1.length();
+        for (int i = 0; i < len; i ++) {
+            if (s1[i] != s2[i]) return s1[i] > s2[i];
+        }
+        return s1.length() > s2.length();
+    }
+    string largestNumber(vector<int> &num) {
+        if (num.size() == 0) return "";
+        sort(num.begin(), num.end(), compare);
+        vector<int>::iterator it;
+        string number = "";
+        for (it = num.begin(); it != num.end(); ++ it) {
+            number = number + to_string(*it);
+        }
+        if (number.length() == 0) return number;
+        int i = 0;
+        while (i < number.length()) {
+            if (number[i] != '0') return number.substr(i, number.length()-i);
+            i ++;
+        }
+        return "0";
+    }
+};
