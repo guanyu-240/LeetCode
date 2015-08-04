a19248b29df2248398690fcd69e0e43ccbc07c99
Guanyu Wang <guanyuwang.cs@gmail.com>
2015-08-03 22:44:50 -0400

    Update Search_For_a_Range.cpp

--- C++/Search_For_a_Range.cpp
+++ C++/Search_For_a_Range.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> searchRange(int A[], int n, int target) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
         int left = 0, right = n - 1;
         int start, end;
         vector<int> ret;
@@ -9,9 +9,9 @@ public:
         bool found = false;
         while (left <= right){
             start = (left + right) / 2;
-            if (A[start] < target) left = start + 1;
-            else if (A[start] > target) right = start - 1;
-            else if (start == 0 || (start != 0 && A[start-1] < target)){
+            if (nums[start] < target) left = start + 1;
+            else if (nums[start] > target) right = start - 1;
+            else if (start == 0 || (start != 0 && nums[start-1] < target)){
                 found = true;
                 break;
             }
@@ -22,12 +22,13 @@ public:
         left = 0; right = n-1;
         while (left <= right){
             end = (left + right) / 2;
-            if (A[end] < target)left = end + 1;
-            else if (A[end] > target) right = end - 1;
-            else if (end == n - 1 || (end != n - 1 && A[end+1] > target)) break;
+            if (nums[end] < target)left = end + 1;
+            else if (nums[end] > target) right = end - 1;
+            else if (end == n - 1 || (end != n - 1 && nums[end+1] > target)) break;
             else left = end + 1;
         }
         ret[1] = end;
         return ret;
     }
 };
+
