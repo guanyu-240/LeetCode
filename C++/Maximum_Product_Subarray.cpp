069c0d6ae498c4ee6edd5e609018f8f71f4f9cc5
Guanyu Wang <guanyuwang.cs@gmail.com>
2015-08-03 22:49:45 -0400

    Update Maximum_Product_Subarray.cpp

--- C++/Maximum_Product_Subarray.cpp
+++ C++/Maximum_Product_Subarray.cpp
@@ -7,7 +7,8 @@ public:
     If positive product is updated, it can be a candidate,
     otherwise, the current element is the candidate
     */
-    int maxProduct(int A[], int n) {
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
         if (n == 0) return 0;
         int maxCurPos = 0, maxPrevPos = 0;
         int minCurNeg = 0, minPrevNeg = 0;
@@ -16,16 +17,16 @@ public:
         for (int i = 0; i < n; i ++) {
             maxPrevPos = maxCurPos;
             minPrevNeg = minCurNeg;
-            if (A[i] >= 0) {
-                maxCurPos = maxPrevPos != 0 ? maxPrevPos*A[i] : A[i];
-                minCurNeg = minPrevNeg * A[i];
+            if (nums[i] >= 0) {
+                maxCurPos = maxPrevPos != 0 ? maxPrevPos*nums[i] : nums[i];
+                minCurNeg = minPrevNeg * nums[i];
                 maxCur = maxCurPos;
             }
             else {
-                maxCurPos = minPrevNeg * A[i];
-                minCurNeg = maxPrevPos != 0 ? maxPrevPos*A[i] : A[i];
+                maxCurPos = minPrevNeg * nums[i];
+                minCurNeg = maxPrevPos != 0 ? maxPrevPos*nums[i] : nums[i];
                 if (maxCurPos > 0) maxCur = maxCurPos;
-                else maxCur = A[i];
+                else maxCur = nums[i];
             }
             ret = max(ret, maxCur);
         }
