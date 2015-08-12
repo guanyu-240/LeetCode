00790d08612bc697f59420dc230d7e5fc0026aae
Guanyu Wang <guanyuwang.cs@gmail.com>
2015-08-11 23:35:22 -0400

    Update Majority_Element.cpp

--- C++/Majority_Element.cpp
+++ C++/Majority_Element.cpp
@@ -5,6 +5,11 @@ public:
         Using Boyer-Moore Majority Voting Algorithm
         Iterate through the array and track the most voted 
         element.
+        Proof of Boyer-Moore Voting Algorithm:
+        Assume the majority element x exists in array A.
+        For any number i in the array A, the vote of i
+        would reach 0 when every i is paired with other
+        numbers, then i can not be majority
         */
         if (nums.size() == 0) return 0;
         int vote = 0, count = 0;
