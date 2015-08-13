577ac2b4aad6e0e3c6e1fbc90ca6921fdb1ecaa9
Guanyu Wang <guanyuwang.cs@gmail.com>
2015-08-13 13:19:46 -0400

    Create Verify_Preorder_Sequence_in_BST.cpp

---
+++ C++/Verify_Preorder_Sequence_in_BST.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        if (preorder.size() <= 1) return true;
+        stack<int> st;
+        int lower_bound = INT_MIN;
+        for (int x : preorder) {
+            if (x < lower_bound) return false;
+            while (!st.empty() && st.top() < x) {
+                lower_bound = st.top();
+                st.pop();
+            }
+            st.push(x);
+        }
+        return true;
+    }
+};
