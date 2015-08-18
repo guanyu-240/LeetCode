4d8a4eefe7f9a7f868934abff61e1aba1124f249
Guanyu Wang <guanyuwang.cs@gmail.com>
2015-08-18 14:34:42 -0400

    Update Graph_Valid_Tree.cpp

--- C++/Graph_Valid_Tree.cpp
+++ C++/Graph_Valid_Tree.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
-        /* BFS */
         if (edges.size() != n-1) return false;
-        vector<vector<int>> adj_table (n, vector<int>());
+        vector<unordered_set<int>> adj_table (n, unordered_set<int>());
         vector<bool> visited(n, false);
         queue<int> q;
         int cur = 0;
         for (pair<int, int> &e : edges) {
-            adj_table[e.first].push_back(e.second);
-            adj_table[e.second].push_back(e.first);
+            adj_table[e.first].insert(e.second);
+            adj_table[e.second].insert(e.first);
         }
         q.push(0);
         while (!q.empty()) {
             cur = q.front();
+            if (visited[cur]) return false;
             q.pop();
             visited[cur] = true;
             for (int v : adj_table[cur]) {
-                if (!visited[v]) {
-                    q.push(v);
-                }
+                adj_table[v].erase(cur);
+                q.push(v);
             }
         }
-        for (bool isVisited : visited) {
-            if (!isVisited) return false;
-        }
+
         return true;
     }
 };
