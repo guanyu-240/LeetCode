9cd9f0663f97cb10d3d78d97b5d7670cbb3b2255
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-06-17 15:08:35 -0400

    Update Design_Twitter.java

--- Java/Design_Twitter.java
+++ Java/Design_Twitter.java
@@ -1,11 +1,63 @@
 public class Twitter {
+    
+    /* members */
+    private HashMap<Integer, User> users;
+    private int ts;
+    
+    /** Initialize your data structure here. */
+    public Twitter() {
+        ts = 0;
+        users = new HashMap<Integer, User>();
+    }
+    
+    /** Compose a new tweet. */
+    public void postTweet(int userId, int tweetId) {
+        /* Add tweet to user-tweet map */
+        if (!users.containsKey(userId)) {
+            users.put(userId, new User());
+        }
+        User u = users.get(userId);
+        u.addTweet(tweetId, ts);
+        ts ++;
+    }
+    
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    public List<Integer> getNewsFeed(int userId) {
+        User u = users.get(userId);
+        if (u == null) return new LinkedList<Integer>();
+        return u.getNewsFeed();
+    }
+    
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    public void follow(int followerId, int followeeId) {
+        if (followerId == followeeId) return;
+        User follower = users.get(followerId), followee = users.get(followeeId);
+        if (follower == null) {
+            follower = new User();
+            users.put(followerId, follower);
+        }
+        if (followee == null) {
+            followee = new User();
+            users.put(followeeId, followee);
+        }
+        followee.addFollower(follower);
+    }
+    
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    public void unfollow(int followerId, int followeeId) {
+        if (followerId == followeeId) return;
+        User follower = users.get(followerId), followee = users.get(followeeId);
+        if (follower == null || followee == null) return;
+        followee.rmFollower(follower);
+    }
+    
+    
+    
     /* Definition of Tweet structure */
-    class Tweet implements Comparable<Tweet>{
-        int userId;
-        int tweetId;
-        int ts;
-        public Tweet (int userId, int tweetId, int ts) {
-            this.userId = userId;
+    private static class Tweet implements Comparable<Tweet>{
+        private int tweetId;
+        private int ts;
+        public Tweet (int tweetId, int ts) {
             this.tweetId = tweetId;
             this.ts = ts;
         }
@@ -14,100 +66,62 @@ public class Twitter {
             return other.ts - ts;
         }
     }
-    /**/
-    
-    /* members */
-    private HashMap<Integer, HashSet<Integer>> followersMap;
-    private HashMap<Integer, PriorityQueue<Tweet>> newsMap;
-    private HashMap<Integer, HashSet<Tweet>> uid_tweetMap;
-    private int ts;
-    
-    /** Initialize your data structure here. */
-    public Twitter() {
-        ts = 0;
-        followersMap = new HashMap<Integer, HashSet<Integer>>();
-        newsMap = new HashMap<Integer, PriorityQueue<Tweet>>();
-        uid_tweetMap = new HashMap<Integer, HashSet<Tweet>>();
-    }
-    
-    /** Compose a new tweet. */
-    public void postTweet(int userId, int tweetId) {
-        Tweet t = new Tweet(userId, tweetId, ts);
-        /* Add tweet to user-tweet map */
-        if (!uid_tweetMap.containsKey(userId)) {
-            uid_tweetMap.put(userId, new HashSet<Tweet>());
+    /* Definition of User structure*/
+    private static class User {
+        private HashSet<User> followers;
+        private PriorityQueue<Tweet> news;
+        private LinkedList<Tweet> tweets;
+        public User(){
+            followers = new HashSet<User>();
+            news = new PriorityQueue<Tweet>();
+            tweets = new LinkedList<Tweet>();
         }
-        HashSet<Tweet> tweets = uid_tweetMap.get(userId);
-        tweets.add(t);
-        /* add the tweet to the news of his/her followers */
-        if (!followersMap.containsKey(userId)) {
-            followersMap.put(userId, new HashSet<Integer>());
-        }
-        HashSet<Integer> followers = followersMap.get(userId);
-        followers.add(userId);
-        for (int follower : followers) {
-            if (!newsMap.containsKey(follower)) {
-                newsMap.put(follower, new PriorityQueue<Tweet>());
+        public void addTweet(int tweetId, int ts) {
+            Tweet t = new Tweet(tweetId, ts);
+            tweets.add(t);
+            news.add(t);
+            for (User u: followers) {
+                u.addToNews(t);
             }
-            PriorityQueue<Tweet> news = newsMap.get(follower);
+        }
+        public List<Integer> getNewsFeed() {
+            List<Integer> ret = new LinkedList<Integer>();
+            if (news == null) return ret;
+            Queue<Tweet> tmp = new LinkedList<Tweet>();
+            int count = 10;
+            while (!news.isEmpty() && count > 0) {
+                ret.add(news.peek().tweetId);
+                tmp.add(news.poll());
+                count --;
+            }
+            
+            while (!tmp.isEmpty()) {
+                news.add(tmp.poll());
+            }
+            return ret;
+        }
+        public void addToNews(Tweet t){
             news.add(t);
         }
-        ts ++;
-    }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    public List<Integer> getNewsFeed(int userId) {
-        List<Integer> ret = new LinkedList<Integer>();
-        PriorityQueue<Tweet> news = newsMap.get(userId);
-        if (news == null) return ret;
-        Queue<Tweet> tmp = new LinkedList<Tweet>();
-        int count = 10;
-        while (!news.isEmpty() && count > 0) {
-            ret.add(news.peek().tweetId);
-            tmp.add(news.poll());
-            count --;
-        }
-        while (!tmp.isEmpty()) {
-            news.add(tmp.poll());
-        }
-        return ret;
-    }
-    
-    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    public void follow(int followerId, int followeeId) {
-        if (followerId == followeeId) return;
-        if (!followersMap.containsKey(followeeId)) {
-            followersMap.put(followeeId, new HashSet<Integer>());
-        }
-        HashSet<Integer> followers = followersMap.get(followeeId);
-        if (followers.contains(followerId)) return;
-        followers.add(followerId);
-        /* Add tweets to follower's news queue */
-        HashSet<Tweet> tweets = uid_tweetMap.get(followeeId);
-        if (tweets == null) return;
-        if (!newsMap.containsKey(followerId)) {
-            newsMap.put(followerId, new PriorityQueue<Tweet>());
-        }
-        PriorityQueue<Tweet> news = newsMap.get(followerId);
-        for (Tweet t : tweets) {
-            news.add(t);
-        }
-    }
-    
-    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-    public void unfollow(int followerId, int followeeId) {
-        if (followerId == followeeId) return;
-        HashSet<Integer> followers = followersMap.get(followeeId);
-        if (followers == null || !followers.contains(followerId)) return;
-        followers.remove(followerId);
-        HashSet<Tweet> tweets = uid_tweetMap.get(followeeId);
-        if (tweets == null) return;
-        PriorityQueue<Tweet> news = newsMap.get(followerId);
-        if (news == null) return;
-        for (Tweet t : tweets) {
+        public void removeFromNews(Tweet t) {
             news.remove(t);
         }
+        public void addFollower(User follower){
+            if (follower == null || followers.contains(follower)) return;
+            followers.add(follower);
+            for (Tweet t : tweets) {
+                follower.addToNews(t);
+            }
+        }
+        public void rmFollower(User follower) {
+            if (follower == null || !followers.contains(follower)) return;
+            followers.remove(follower);
+            for (Tweet t : tweets) {
+                follower.removeFromNews(t);
+            }
+        }
     }
+    /**/
 }
 
 /**
