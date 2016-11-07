0ebf833a34e063fbc2d33f54f577cbabe8ccbd74
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-11-07 17:56:49 -0500

    Update Convert_Number_to_Hex.java

--- Java/Convert_Number_to_Hex.java
+++ Java/Convert_Number_to_Hex.java
@@ -1,16 +1,14 @@
 public class Solution {
-    private char toHexChar(long x) {
+    private char toHexChar(int x) {
         if (x > 15 || x < 0) return '\0';
-        else if (x < 10) return (char)('0'+(int)x);
-        else return (char)('a'+(int)(x-10));
+        else if (x < 10) return (char)('0'+x);
+        else return (char)('a'+x-10);
     }
     public String toHex(int num) {
-        long x = ~((long)1 << 63) & num;
         StringBuilder sb = new StringBuilder();
-        while (x != 0) {
-            sb.append(toHexChar(x%16));
-            x /= 16;
-            if (sb.length() == 8) break;
+        while (num != 0) {
+            sb.append(toHexChar(num&15));
+            num >>>= 4;
         }
         if (sb.length() == 0) sb.append('0');
         return sb.reverse().toString();
